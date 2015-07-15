1107917e74d490bfebc4f1671e25ef3c83287e0f
Janek Sprenger <janek.sprenger@sunrise.ch>
2015-07-15 12:10:55 +0200

    Struktur eingebaut, Design

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -21,13 +8,11 @@ var app = {
         }
     },
 
-
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 
