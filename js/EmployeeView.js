851b0cdac3efa474f77480f9a9a4c5f9422c4eff
Janek Sprenger <janek.sprenger@sunrise.ch>
2015-07-15 14:08:02 +0200

    Kontakt in Adressbuch exportieren funzt nicht Neu sollte eine Kontakbild aufgenommen werden können

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -19,12 +19,11 @@ var EmployeeView = function (employee) {
     this.addToContacts = function(event) {
         event.preventDefault();
         console.log('addToContacts');
-        /*
+        // geht leider nicht auf dem Smartphone, navigator.contacts ist false...
         if (!navigator.contacts) {
             app.showAlert("Contacts API not supported", "Error");
             return;
         }
-        */
         var contact = navigator.contacts.create();
         contact.name = {givenName: employee.firstName, familyName: employee.lastName};
         var phoneNumbers = [];
@@ -35,11 +34,36 @@ var EmployeeView = function (employee) {
         return false;
     };
 
+    this.changePicture = function(event) {
+        event.preventDefault();
+        if (!navigator.camera) {
+            app.showAlert("Camera API not supported", "Error");
+            return;
+        }
+        var options =   {   quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+            encodingType: 0     // 0=JPG 1=PNG
+        };
+
+        navigator.camera.getPicture(
+            function(imageData) {
+                $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+            },
+            function() {
+                app.showAlert('Error taking picture', 'Error');
+            },
+            options);
+
+        return false;
+    };
+
     this.initialize = function () {
         this.el = $('<div/>');
         // register eventlisteners
         this.el.on('click', '.add-location-btn', this.addLocation);
         this.el.on('click', '.add-contact-btn', this.addToContacts);
+        this.el.on('click', '.change-pic-btn', this.changePicture);
     };
 
     this.initialize();
